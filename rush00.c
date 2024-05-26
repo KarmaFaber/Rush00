d631e2aa8e1abfb3507d3fdb5c2a4893bf4233c5
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-05-26 17:18:01 +0200

    Add files via upload

--- rush00.c
+++ rush00.c
@@ -6,7 +6,7 @@
 /*   By: mrubal-c <mrubal-c@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/25 15:46:13 by mrubal-c          #+#    #+#             */
-/*   Updated: 2024/05/25 16:42:21 by mrubal-c         ###   ########.fr       */
+/*   Updated: 2024/05/25 18:21:23 by mrubal-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,39 @@
 
 void	ft_putchar(int x, char left, char mid, char right);
 
-void	rush(int x, int y)
+int    errorchecker(int x, int y)
 {
-	int	row;
+        if (x <= 0 || y <= 0)
+    {
+        write (1, "That's not possible!", 20);
+        return (-1);
+    }
+    return (0);
 
-	row = 1;
-	if (x > 0 && y > 0)
-	{
-		ft_putchar(x, 'o', '-', 'o');
-		write (1, "\n", 1);
-	}
-	while (x > 0 && row < y - 1)
-	{
-		ft_putchar(x, '|', ' ', '|');
-		write (1, "\n", 1);
-		row++;
-	}	
-	if (y > 1)
-	{
-		ft_putchar(x, 'o', '-', 'o');
-		write (1, "\n", 1);
-	}
+}
+void    rush(int x, int y)
+{
+        if (errorchecker(x, y) == -1)
+            return ;
+
+        int     row;
+
+        row = 1;
+        if (x > 0 && y > 0)
+        {
+                ft_putchar(x, '/', '*', '\\');
+                write (1, "\n", 1);
+        }
+        while (x > 0 && row < y - 1)
+        {
+                ft_putchar(x, '*', ' ', '*');
+                write (1, "\n", 1);
+                row++;
+        }
+        if (y > 1)
+        {
+                ft_putchar(x, '\\', '*', '/');
+                write (1, "\n", 1);
+        }
+
 }
